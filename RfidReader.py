2cca0af02784cfdb7e8f92676cfe131bda669f96
Kevin Martins <kevinmmartins@gmmail.com>
2018-08-05 21:32:32 -0300

    Created barcode file and change RFID read

--- RfidReader.py
+++ RfidReader.py
@@ -1,25 +1,25 @@
 import serial
 import RPi.GPIO as GPIO
 import logging
+import traceback
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO);
+logger = logging.getLogger(__name__);
 logger.info('Starting...');
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
-GPIO.output(23, False)
-GPIO.output(24, False)
 PortRF = serial.Serial('/dev/ttyAMA0', 9600)
 
 logger.info('Setup completed successfully !');
 
 while True:
-    ID = ""
-    read_byte = PortRF.read()
-    if read_byte == "\x02":
-        for Counter in range(12):
-            read_byte = PortRF.read()
-            ID += str(read_byte)
-            logger.info(hex(ord(read_byte)));
-        logger.info(ID);
+    try:
+        ID = ""
+        read_byte = PortRF.read();
+        if read_byte == "\x02":
+            for Counter in range(12):
+                read_byte = PortRF.read();
+                ID += str(read_byte);
+                logger.info(hex(ord(read_byte)));
+            logger.info(ID);
+    except:
+        logger.error('Unexpected error: ' + traceback.format_exc());
+        raise;
