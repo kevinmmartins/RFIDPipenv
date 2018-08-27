8bb0c8caead2c7adad20335fe7040e6601d8acbe
Kevin Martins <kevinmmartins@gmmail.com>
2018-08-26 23:19:01 -0300

    Fix serial reader to ten

--- Rfid.py
+++ Rfid.py
@@ -8,7 +8,7 @@ class Rfid:
         logging.basicConfig(level=logging.INFO);
         self.logger = logging.getLogger(__name__);
         self.logger.info('Starting...');
-        self.portRF = serial.Serial('/dev/serial0', 9600)
+        self.portRF = serial.Serial('/dev/serial0', 115200)
         self.logger.info('Setup completed successfully !');
 
     def read(self):
@@ -17,10 +17,9 @@ class Rfid:
                 id = ""
                 read_byte = self.portRF.read();
                 if read_byte == "\x02":
-                    for Counter in range(12):
-                        read_byte = self.portRF.read()
-                        id += str(read_byte)
-                        self.logger.info(hex(ord(read_byte)));
+                    for Counter in range(10):
+                        read_byte = self.portRF.read();
+                        id += str(read_byte);
                     self.logger.info(id);
         except:
             self.logger.error('Unexpected error: ' + traceback.format_exc());
