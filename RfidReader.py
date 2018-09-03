2ca743b3614177aa99e5188590d578b012d1dc5e
Kevin Martins <kevinmmartins@gmmail.com>
2018-09-02 22:14:34 -0300

    Fix main class

--- RfidReader.py
+++ RfidReader.py
@@ -1,10 +1,14 @@
 from BarCode import BarCode
 from Rfid import Rfid
 
-if __name__ == "__main__":
+def main():
     while True:
         reader= Rfid()
         barcodeStore= BarCode()
         reader.read()
         barcodeStore.add_product()
+
+if __name__ == "__main__":
+    main()
+
 
