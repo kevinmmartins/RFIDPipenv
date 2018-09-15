80165299d7a64e5b30830c0cafafd304e170f849
Kevin Martins <kevinmmartins@gmmail.com>
2018-09-15 18:49:00 -0300

    Fix server

--- server/ServerRFID.py
+++ server/ServerRFID.py
@@ -3,16 +3,31 @@
 import SimpleHTTPServer
 import SocketServer
 import RFID
-
-PORT = 8000
+import traceback
+import json
 
 class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-	tag = RFID.read_serial()
-        print tag
-        self.wfile.write(tag)
-        # self.send_response(200, 'OK')
+        try:
+            tag = RFID.read_serial()
+            print tag
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({'RFID': tag}))
+        except:
+            print "Unexpected error: " + traceback.format_exc()
+            self.send_response(500)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
 
-Handler = GetHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+def main():
+    PORT = 8000
+    Handler = GetHandler
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+    print "Serving at port ",PORT
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
